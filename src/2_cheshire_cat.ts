3f311142378a2d77c2e9d2035afd742f7a5814f2
Cenk <muzzyathome@gmail.com>
2022-03-07 15:02:30 +0000

    Past the cheshire cat

--- src/2_cheshire_cat.ts
+++ src/2_cheshire_cat.ts
@@ -22,6 +22,7 @@ export function meetTheCheshireCat(): void {
 	const theCat = new CheshireCat();
 
 	// 👉 FIXME ❌
+	theCat.expression = '😁 Grin 😁';
 	// 		Add some code here which allows you to progress *without* altering any other code in this file
 	// 👉 END FIXME ❌
 
